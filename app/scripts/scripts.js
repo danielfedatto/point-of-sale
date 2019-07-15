49b60e41153619de4f0d043b88441c96156a1c45
Daniel Fedatto <daniel.fedatto@gmail.com>
2019-07-15 00:34:19 -0300

    Correção de bug no slick slider

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -9,6 +9,8 @@ $(document).ready(function(){
         dots: false,
         arrows: true,
         slidesToShow: 5,
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>',
         responsive: [
             {
               breakpoint: 1024,
